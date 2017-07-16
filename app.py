d36011867820b8c9f614c65518f2f6db0a8c2833
Jose <o.castillojl@gmail.com>
2017-07-16 00:54:12 -0500

    update 1.0

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
-from flask import Flask, render_template, flash, redirect, url_for, session,logging,request
+from flask import Flask, render_template, flash, redirect, url_for, session, request, logging
 from data import Articles
 from flask_mysqldb import MySQL
-from wtforms import  Form, StringField, TextAreaField, PasswordField, validators
+from wtforms import Form, StringField, TextAreaField, PasswordField, validators
 from passlib.hash import sha256_crypt
+from functools import wraps
 
 DEBUG = True
 
@@ -51,9 +52,30 @@ class RegisterForm(Form):
 def register():
     form = RegisterForm(request.form)
     if request.method ==  'POST' and form.validate():
-        return render_template('register.html')
+        name = form.name.data
+        email = form.email.data
+        username = form.username.data
+        password = sha256_crypt.encrypt(str(form.password.data))
+
+        #create cursor
+        cur = mysql.connection.cursor()
+
+        # Execute query
+        cur.execute("INSERT INTO users(name, email, username, password) VALUES(%s, %s, %s, %s)", (name, email, username, password))
+
+       # Commit to DB
+        mysql.connection.commit()
+
+        # Close connection
+        cur.close()
+
+        flash('You are now registered and can log in', 'success')
+
+        return redirect(url_for('login'))
+
     return render_template('register.html',form=form)
 
 if __name__ == '__main__':
     app.debug = DEBUG
+    app.secret_key = 'qwerty12345'
     app.run()
